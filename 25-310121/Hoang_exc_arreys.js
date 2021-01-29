f0771cc2df555e564722449c4d7c1817c10e25d9
Järventie Laura <s2100135@edu.bc.fi>
2021-01-29 08:49:32 +0200

    another try

--- 25-310121/Hoang_exc_arreys.js
+++ 25-310121/Hoang_exc_arreys.js
@@ -1,17 +1,48 @@
-let chosenNumber = Math.floor(Math.random()*2);
-let secondchosenNumber = Math.floor(Math.random()*2);
-let thirdchosenNumber = Math.floor(Math.random()*2);
-let fourthchosenNumber = Math.floor(Math.random()*2);
-let fifthchosenNumber = Math.floor(Math.random()*2);
 
-let listOfNumbers = [chosenNumber, secondchosenNumber, thirdchosenNumber, fourthchosenNumber, fifthchosenNumber];
-console.log(listOfNumbers);
+let playedRounds = 0;
+let jackpotStatus = false;
+let roundCost = 0.5;
 
+while (jackpotStatus === false) { 
+    playedRounds = playedRounds + 1;
 
-let numbers = [listOfNumbers];
+let lotteryNumbers = [];
+let counter = 0;
 
-let count = 1;
-while (count < 10) {
-    console.log(count);
-    count +=2;
+while (counter <5){
+    lotteryNumbers[counter] = Math.floor(Math.random()*6);
+    counter = counter + 1
 }
+console.log(lotteryNumbers)
+
+let oneCounter = 0;
+let zeroCounter = 0;
+counter = 0;
+
+while (counter < 5) {
+
+   if (lotteryNumbers[counter] === 1) {
+       oneCounter = oneCounter + 1;
+   }
+   else if (lotteryNumbers[counter] ===0) {
+       zeroCounter = zeroCounter + 1;
+   }
+   counter = counter + 1;
+}
+
+console.log('You got number one ' + oneCounter + ' times');
+console.log('You got number zero ' + zeroCounter + ' times');
+
+if (oneCounter === 5) {
+    jackpotStatus = true;
+    console.log('JACKPOT')
+} else if (zeroCounter === 5) {
+    console.log('Congrats, but no win')
+} else if (oneCounter === 4) {
+    console.log('small price')
+}
+}
+console.log(playedRounds);
+console.log( 'You have spend ' + roundCost*playedRounds + ' € to win the jackpot.' );
+
+
