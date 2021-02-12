83f5be3404277e66fc7f759be55f641bf8edf0b2
Laura Järventie <s2100135@edu.bc.fi>
2021-02-12 21:30:07 +0200

    fri

---
+++ 08-140221/testi.js
@@ -0,0 +1,47 @@
+let fifthArray = [
+  1,
+  500,
+  10000,
+  "world",
+  "yeah",
+  "bye bye",
+  123456,
+  234,
+  500,
+  10000,
+  12345,
+  20000,
+  true,
+  true,
+  false,
+  "hello",
+  "world",
+];
+
+fifthArray.sort();
+
+let modifiedArrey = [];
+
+fifthArray.forEach((fifthArray[i] === fifthArray[i]) => {
+    if {
+         === 
+    }
+});
+
+let numberInRange = fifthArray.filter(range);
+console.log(numberInRange);
+
+/*
+for (let i = 0; i < fifthArray.length; i++) {
+  if (fifthArray[i] === fifthArray[i]) {
+    delete fifthArray[i];
+  }
+}
+
+
+
+if ((a, b)) fifthArray.sort();
+fifthArray.filter((a) => {
+  return a === "a";
+});
+*/
